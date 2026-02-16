516e7035b8ebecb0f614b49e72e74635ecca83ff
Ronald Foong <rffoong.student.unimelb.edu.au>
2026-02-16 12:49:11 +1100

    Add properties to Person

--- src/main/java/app/Person.java
+++ src/main/java/app/Person.java
@@ -1,8 +1,15 @@
 package app;
 
-import java.util.Scanner;
+import java.util.Date;
 
 public class Person {
+	private String firstName;
+	private String lastName;
+	private String personID;
+	private String address;
+	private Date birthDate;
+	private int age;
+
 	public static void addPerson() {
 
 	}
