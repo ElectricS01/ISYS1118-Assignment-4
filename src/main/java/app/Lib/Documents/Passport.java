c8e02581afdb72d4f877a3fff0dfe3cfb9a67dde
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2026-02-20 21:45:06 +1100

    Update Passport with csv methods

--- src/main/java/app/Lib/Documents/Passport.java
+++ src/main/java/app/Lib/Documents/Passport.java
@@ -2,33 +2,34 @@ package app.Lib.Documents;
 
 public class Passport extends IdDocument {
 
-	private final String authority;
+  private final String authority;
 
-	public Passport(
-			String id,
-			String name,
-			String dateOfBirth,
-			String country,
-			String dateOfIssue,
-			String dateOfExpiry,
-			String authority
-	) {
-		super(id, name, dateOfBirth, country, dateOfIssue, dateOfExpiry);
-		this.authority = authority;
-	}
+  public Passport(
+      String id,
+      String name,
+      String dateOfBirth,
+      String country,
+      String dateOfIssue,
+      String dateOfExpiry,
+      String authority) {
+    super(id, name, dateOfBirth, country, dateOfIssue, dateOfExpiry);
+    this.authority = authority;
+  }
 
-	@Override
-	public boolean isValid() {
-		return id.matches("^[A-Za-z]{2}\\d{6}$");
-	}
+  @Override
+  public boolean isValid() {
+    if (!areDatesValid()) return false;
 
-	@Override
-	protected String extraCsvHeader() {
-		return ",authority";
-	}
+    return id.matches("^[A-Za-z]{2}\\d{6}$");
+  }
 
-	@Override
-	protected String extraCsvFields() {
-		return "," + authority;
-	}
+  @Override
+  protected String extraCsvHeader() {
+    return ",authority";
+  }
+
+  @Override
+  protected String extraCsvFields() {
+    return "," + authority;
+  }
 }
