28657156c4e31f9667171781bf3ab631f4c11438
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2026-02-20 18:57:41 +1100

    Add CSV helpers and country field to IdDocument

--- src/main/java/app/Lib/Documents/IdDocument.java
+++ src/main/java/app/Lib/Documents/IdDocument.java
@@ -1,29 +1,57 @@
 package app.Lib.Documents;
 
 public abstract class IdDocument {
+
+	protected String id;
+	protected String name;
+	protected String dateOfBirth;
+	protected String country;
+	protected String dateOfIssue;
+	protected String dateOfExpiry;
+
 	public IdDocument(
 			String id,
 			String name,
 			String dateOfBirth,
+			String country,
 			String dateOfIssue,
 			String dateOfExpiry
 	) {
 		this.id = id;
 		this.name = name;
 		this.dateOfBirth = dateOfBirth;
+		this.country = country;
 		this.dateOfIssue = dateOfIssue;
 		this.dateOfExpiry = dateOfExpiry;
 	}
 
-	String id;
-	String name;
-	String dateOfBirth;
-	String dateOfIssue;
-	String dateOfExpiry;
-
 	public abstract boolean isValid();
 
+	public boolean childrenOnly() {
+		return false;
+	}
+
+	protected String extraCsvHeader() {
+		return "";
+	}
+
+	protected String extraCsvFields() {
+		return "";
+	}
+
+	public final String getCsvHeader() {
+		return "id,name,dateOfBirth,country,dateOfIssue,dateOfExpiry"
+				+ extraCsvHeader();
+	}
+
+	public final String toCsvRow() {
+		return id + "," + name + "," + dateOfBirth + "," +
+				country + "," + dateOfIssue + "," + dateOfExpiry
+				+ extraCsvFields();
+	}
+
 	public boolean save() {
+		//TODO: Implement save to text file or csv file
 		return true;
 	}
 }
